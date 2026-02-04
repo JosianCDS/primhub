1ec011044c4244a6eb0edce6a47d94504eba2ec9
AlexanderPrado <alexanderprado2101@gmail.com>
2026-02-04 14:14:29 -0500

    Pre Descarga

--- lib/endpoint/endpoint.dart
+++ lib/endpoint/endpoint.dart
@@ -6,10 +6,22 @@ class Endpoint {
   static String productChip = "$baseUrl/api/v1/models/C_BPartner_Product_Chip";
   static String cBPartner = "$baseUrl/api/v1/models/C_BPartner";
   static String adUser = "$baseUrl/api/v1/models/AD_User";
-  static String project = "$baseUrl/api/v1/models/C_Project";  // Auth Endpoints
+  static String project = "$baseUrl/api/v1/models/C_Project"; // Auth Endpoints
+  static String primDocuments = "$baseUrl/api/v1/models/PRIM_Documents";
   static String authTokens = "$baseUrl/api/v1/auth/tokens";
   static String authRoles = "$baseUrl/api/v1/auth/roles";
   static String authOrgs = "$baseUrl/api/v1/auth/organizations";
   static String authWarehouses = "$baseUrl/api/v1/auth/warehouses";
   static String authLogout = "$baseUrl/api/v1/auth/logout";
+ static String primDocumentsRelated = "$baseUrl/api/v1/models/PRIM_Documents_Related";
+}
+
+
+class PostMedia {
+  final int recordID;
+  final String tableName;
+
+  PostMedia({required this.recordID, required this.tableName});
+
+  String get endPoint => '$tableName/$recordID/attachments';
 }
